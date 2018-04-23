4c58587243a21ed9f799701b6d94e256fdee9d42
LDmicroGitHub <LDmicroGitHub@gmail.com>
2018-04-23 11:11:02 +0300

    clang

---
+++ ldmicro/clang.bat
@@ -0,0 +1,6 @@
+clang-format.exe -style=file     %1 > %1.file
+clang-format.exe -style=LLVM     %1 > %1.LLVM
+clang-format.exe -style=Google   %1 > %1.Google
+clang-format.exe -style=Chromium %1 > %1.Chromium
+clang-format.exe -style=Mozilla  %1 > %1.Mozilla
+clang-format.exe -style=Webkit   %1 > %1.Webkit
