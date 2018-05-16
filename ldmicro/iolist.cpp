379e9be16a595dfa73c97f933980c349f7c3340f
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2018-05-16 14:11:56 +0300

    revert few changes.

--- ldmicro/iolist.cpp
+++ ldmicro/iolist.cpp
@@ -699,13 +699,10 @@ int GenerateIoList(int prevSel)
     for(i = 0; i < Prog.numRungs; i++) {
         ExtractNamesFromCircuit(ELEM_SERIES_SUBCKT, Prog.rungs[i]);
     }
-    char _tmp[128];
-    strcpy(_tmp, "ROverflowFlagV");
-    AppendIo(_tmp, IO_TYPE_INTERNAL_RELAY);
+    AppendIo("ROverflowFlagV", IO_TYPE_INTERNAL_RELAY);
 
     if(Prog.cycleDuty) {
-        strcpy(_tmp, YPlcCycleDuty);
-        AppendIo(_tmp, IO_TYPE_DIG_OUTPUT);
+        AppendIo(YPlcCycleDuty, IO_TYPE_DIG_OUTPUT);
     }
     for(i = 0; i < Prog.io.count; i++) {
         // clang-format off
