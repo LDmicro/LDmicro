b67421be69d1a7fbda42e374897e8c3a6ada044a
LDmicroGitHub <LDmicroGitHub@gmail.com>
2019-02-17 00:10:45 +0200

    #define OOPS_AS_THROW

--- ldmicro/compilerexceptions.hpp
+++ ldmicro/compilerexceptions.hpp
@@ -16,12 +16,13 @@ extern int CompileFailure;      ///// added by JG
 
 #ifdef USE_JG
 #else
-#define THROW_COMPILER_EXCEPTION(MSG, ...) do{ \
-    char message[1024*3];\
-    sprintf(message, "%s\n[%i:%s]", MSG, __LINE__, __LDLOG_LOG_FILE); \
-    throw std::runtime_error(message);\
-    }while(0)
+    #define THROW_COMPILER_EXCEPTION(MSG, ...) do{ \
+        char message[1024*3];\
+        sprintf(message, "%s\n[%i:%s]", MSG, __LINE__, __LDLOG_LOG_FILE); \
+        throw std::runtime_error(message);\
+        }while(0)
 #endif
+
 #ifdef USE_JG
 /////   variable list of args added
 /////   throw std::runtime_error(message) \;
@@ -30,31 +31,31 @@ extern int CompileFailure;      ///// added by JG
 /////   CompileFailure= 1; \
 /////   return __VA_ARGS__; \
 
-#define THROW_COMPILER_EXCEPTION(MSG, ...) do{ \
-    char message[1024*3]; \
-    sprintf(message, "%s\n[%i:%s]", MSG, __LINE__, __LDLOG_LOG_FILE); \
-    Error(message); \
-    CompileFailure= 1; \
-    return __VA_ARGS__; \
-    }while(0)
+    #define THROW_COMPILER_EXCEPTION(MSG, ...) do{ \
+        char message[1024*3]; \
+        sprintf(message, "%s\n[%i:%s]", MSG, __LINE__, __LDLOG_LOG_FILE); \
+        Error(message); \
+        CompileFailure= 1; \
+        return __VA_ARGS__; \
+        }while(0)
 
 #else
-#define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
-    char format[1024];\
-    sprintf(format, FMT, __VA_ARGS__); \
-    char message[1024*3];\
-    sprintf(message, "%s\n[%i:%s]", format, __LINE__, __LDLOG_LOG_FILE); \
-    throw std::runtime_error(message);\
-    }while(0)
+    #define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
+        char format[1024];\
+        sprintf(format, FMT, __VA_ARGS__); \
+        char message[1024*3];\
+        sprintf(message, "%s\n[%i:%s]", format, __LINE__, __LDLOG_LOG_FILE); \
+        throw std::runtime_error(message);\
+        }while(0)
 #endif
 
 #ifdef USE_JG
-#define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
-    char format[1024]; \
-    sprintf(format, FMT, __VA_ARGS__); \
-    char message[1024*3];\
-    sprintf(message, "%s\n[%i:%s]", format, __LINE__, __LDLOG_LOG_FILE); \
-    Error(message); \
-    CompileFailure= 1; \
-    }while(0)
+    #define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
+        char format[1024]; \
+        sprintf(format, FMT, __VA_ARGS__); \
+        char message[1024*3];\
+        sprintf(message, "%s\n[%i:%s]", format, __LINE__, __LDLOG_LOG_FILE); \
+        Error(message); \
+        CompileFailure= 1; \
+        }while(0)
 #endif
