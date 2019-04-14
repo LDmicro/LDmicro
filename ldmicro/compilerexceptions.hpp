57b3f0e4897028bd57ce0346987717b6c8f73ed6
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2019-04-14 22:49:34 +0300

    compilerexceptions: clean up

--- ldmicro/compilerexceptions.hpp
+++ ldmicro/compilerexceptions.hpp
@@ -6,60 +6,22 @@
 #define RM_SLASH(str) (strstr(str, "/") != NULL ? strrchr(str, '/') + 1 : \
                        strstr(str, "\\") != NULL ? strrchr(str, '\\') + 1 : str)
 
-//#ifndef __LLFILE__
 
 #define __LLFILE__ RM_SLASH(__FILE__)
 
-//#endif
-
-//#define USE_JG
-
 extern int CompileFailure;      ///// added by JG
 
-#ifdef USE_JG
-#else
-    #define THROW_COMPILER_EXCEPTION(MSG) do{ \
-        char __message[1024*3];\
-        sprintf(__message, "%s[%i:%s]", MSG, __LINE__, __LLFILE__); \
-        throw std::runtime_error(__message);\
-        }while(0)
-#endif
+#define THROW_COMPILER_EXCEPTION(MSG) do{ \
+    char __message[1024*4];\
+    sprintf(__message, "%s[%i:%s]", MSG, __LINE__, __LLFILE__); \
+    throw std::runtime_error(__message);\
+    }while(0)
 
-#ifdef USE_JG
-/*
-/////   variable list of args added
-/////   throw std::runtime_error(message) \;
-/////   replaced twice above by :                                   // by JG
-/////   Error(message); \
-/////   CompileFailure= 1; \
-/////   return __VA_ARGS__; \
-*/
+#define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
+    char __message[1024*4];\
+    char __format[1024];\
+    sprintf(__format, (FMT), __VA_ARGS__); \
+    sprintf(__message, "%s[%i:%s]", __format, __LINE__, __LLFILE__); \
+    throw std::runtime_error(__message);\
+    }while(0)
 
-    #define THROW_COMPILER_EXCEPTION(MSG, ...) do{ \
-        char message[1024*3]; \
-        sprintf(message, "%s\n[%i:%s]", MSG, __LINE__, __LLFILE__); \
-        Error(message); \
-        CompileFailure= 1; \
-        return __VA_ARGS__; \
-        }while(0)
-
-#else
-    #define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
-        char __format[1024];\
-        sprintf(__format, (FMT), __VA_ARGS__); \
-        char __message[1024*3];\
-        sprintf(__message, "%s[%i:%s]", __format, __LINE__, __LLFILE__); \
-        throw std::runtime_error(__message);\
-        }while(0)
-#endif
-
-#ifdef USE_JG
-    #define THROW_COMPILER_EXCEPTION_FMT(FMT,...) do{ \
-        char format[1024]; \
-        sprintf(format, FMT, __VA_ARGS__); \
-        char message[1024*3];\
-        sprintf(message, "%s\n[%i:%s]", format, __LINE__, __LLFILE__); \
-        Error(message); \
-        CompileFailure= 1; \
-        }while(0)
-#endif
