2894771621337cf6833a23b6bb55d026a0335528
Ihor_Nehrutsa <ihor.nehrutsa@gmail.com>
2019-06-03 16:49:20 +0300

    WIP

---
+++ ldmicro/LIBRARIES_FOR/Avr/UartLib.c
@@ -0,0 +1,36 @@
+#include "ladder.h"
+
+#include "UartLib.h"
+
+void UART_Init(unsigned char divisor, unsigned char brgh) {
+  // UART baud rate setup
+  SPBRG = divisor;
+  BRGH = brgh != 0;
+  TXEN = 1; CREN = 1; SPEN = 1;
+}
+
+void UART_Transmit(unsigned char data) {
+  // Wait for empty transmit buffer
+  while( !TRMT );
+  // Put data into buffer, sends the data
+  TXREG = data;
+}
+
+unsigned char UART_Receive(void) {
+  // Wait for data to be received
+  while( !RCIF );
+  // Get and return received data from buffer
+  return RCREG;
+}
+
+ldBOOL UART_Transmit_Ready(void) {
+  return TRMT;
+}
+
+ldBOOL UART_Transmit_Busy(void) {
+  return ! TRMT;
+}
+
+ldBOOL UART_Receive_Avail(void) {
+  return RCIF;
+}
