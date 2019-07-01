57afa49cfd009551b6b4934293103402a31c3e9f
Ihor_Nehrutsa <ihor.nehrutsa@gmail.com>
2019-07-01 16:56:03 +0300

    variant 2 of delay_us() for STM32F1

--- ldmicro/LIBRARIES_FOR/Arm/STM32F1/core_cm3.h
+++ ldmicro/LIBRARIES_FOR/Arm/STM32F1/core_cm3.h
@@ -1695,9 +1695,9 @@ static __INLINE uint32_t SysTick_Config(uint32_t ticks)
 {
   if (ticks > SysTick_LOAD_RELOAD_Msk)  return (1);            /* Reload value impossible */
 
-  SysTick->VAL   = 0;                                          /* Load the SysTick Counter Value */
   SysTick->LOAD  = (ticks & SysTick_LOAD_RELOAD_Msk) - 1;      /* set reload register */
   NVIC_SetPriority (SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);  /* set Priority for Cortex-M0 System Interrupts */
+  SysTick->VAL   = 0;                                          /* Load the SysTick Counter Value */
   SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
                    SysTick_CTRL_TICKINT_Msk   |
                    SysTick_CTRL_ENABLE_Msk;                    /* Enable SysTick IRQ and SysTick Timer */
