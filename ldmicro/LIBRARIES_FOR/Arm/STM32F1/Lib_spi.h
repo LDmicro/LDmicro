3c3dc9e42eca1ab451bcda69bafa9bee3aba3732
Ihor_Nehrutsa <ihor.nehrutsa@gmail.com>
2019-07-02 12:50:53 +0300

    fix warnings in LIBRARIES_FOR/Arm/STM32F1

--- ldmicro/LIBRARIES_FOR/Arm/STM32F1/Lib_spi.h
+++ ldmicro/LIBRARIES_FOR/Arm/STM32F1/Lib_spi.h
@@ -14,13 +14,13 @@ extern C {
  * It supports all 2 SPIs in master mode with 3 Lines Full Duplex mode
  *
  * All 2 SPIs work the same principle by default:
- *	- Master mode (SPI_Mode_Master)
- *	- 8 or 16 data bits (SPI_DataSize_8b or SPI_DataSize_8b)
- *	- Clock polarity low, data sampled at first edge, SPI mode 0 (LibSPI_Mode_0)
- *	- Prescaler from 2 to 256 (SPI_BaudRatePrescaler_2 to SPI_BaudRatePrescaler_256)
- *	- Firstbit MSB (SPI_FirstBit_MSB)
- *	- Software SS pin configure
- *	- Direction is full duplex 3 wires
+ *  - Master mode (SPI_Mode_Master)
+ *  - 8 or 16 data bits (SPI_DataSize_8b or SPI_DataSize_8b)
+ *  - Clock polarity low, data sampled at first edge, SPI mode 0 (LibSPI_Mode_0)
+ *  - Prescaler from 2 to 256 (SPI_BaudRatePrescaler_2 to SPI_BaudRatePrescaler_256)
+ *  - Firstbit MSB (SPI_FirstBit_MSB)
+ *  - Software SS pin configure
+ *  - Direction is full duplex 3 wires
  *
 
     SPIX    MOSI    MISO    SCK     SS
@@ -41,10 +41,10 @@ extern C {
  */
 typedef enum
     {
-	LibSPI_Mode_0, /*!< Clock polarity low, clock phase 1st edge */
-	LibSPI_Mode_1, /*!< Clock polarity low, clock phase 2nd edge */
-	LibSPI_Mode_2, /*!< Clock polarity high, clock phase 1st edge */
-	LibSPI_Mode_3  /*!< Clock polarity high, clock phase 2nd edge */
+    LibSPI_Mode_0, /*!< Clock polarity low, clock phase 1st edge */
+    LibSPI_Mode_1, /*!< Clock polarity low, clock phase 2nd edge */
+    LibSPI_Mode_2, /*!< Clock polarity high, clock phase 1st edge */
+    LibSPI_Mode_3  /*!< Clock polarity high, clock phase 2nd edge */
     } LibSPI_Mode_t;
 
 
@@ -53,8 +53,8 @@ typedef enum
  */
 typedef enum
     {
-	LibSPI_DataSize_8b, /*!< SPI in 8-bits mode */
-	LibSPI_DataSize_16b /*!< SPI in 16-bits mode */
+    LibSPI_DataSize_8b, /*!< SPI in 8-bits mode */
+    LibSPI_DataSize_16b /*!< SPI in 16-bits mode */
     } LibSPI_DataSize_t;
 
 #define USE_SPI1
@@ -63,38 +63,38 @@ typedef enum
 //----- SPI1 options start -------
 //Options can be overwriten in defines.h file
 #ifndef LibSPI1_PRESCALER
-#define LibSPI1_PRESCALER	SPI_BaudRatePrescaler_32
+#define LibSPI1_PRESCALER   SPI_BaudRatePrescaler_32
 #endif
 //Specify datasize
 #ifndef LibSPI1_DATASIZE
-#define LibSPI1_DATASIZE 	SPI_DataSize_8b
+#define LibSPI1_DATASIZE    SPI_DataSize_8b
 #endif
 //Specify which bit is first
 #ifndef LibSPI1_FIRSTBIT
-#define LibSPI1_FIRSTBIT 	SPI_FirstBit_MSB
+#define LibSPI1_FIRSTBIT    SPI_FirstBit_MSB
 #endif
 //Mode, master or slave
 #ifndef LibSPI1_MASTERSLAVE
-#define LibSPI1_MASTERSLAVE	SPI_Mode_Master
+#define LibSPI1_MASTERSLAVE SPI_Mode_Master
 #endif
 //Specify mode of operation, clock polarity and clock phase
 #ifndef LibSPI1_MODE
-#define LibSPI1_MODE		LibSPI_Mode_0
+#define LibSPI1_MODE        LibSPI_Mode_0
 #endif
 //----- SPI1 options end -------
 
 //----- SPI2 options start -------
 //Options can be overwriten in defines.h file
 #ifndef LibSPI2_PRESCALER
-#define LibSPI2_PRESCALER	SPI_BaudRatePrescaler_32
+#define LibSPI2_PRESCALER   SPI_BaudRatePrescaler_32
 #endif
 //Specify datasize
 #ifndef LibSPI2_DATASIZE
-#define LibSPI2_DATASIZE 	SPI_DataSize_8b
+#define LibSPI2_DATASIZE    SPI_DataSize_8b
 #endif
 //Specify which bit is first
 #ifndef LibSPI2_FIRSTBIT
-#define LibSPI2_FIRSTBIT 	SPI_FirstBit_MSB
+#define LibSPI2_FIRSTBIT    SPI_FirstBit_MSB
 #endif
 //Mode, master or slave
 #ifndef LibSPI2_MASTERSLAVE
@@ -102,7 +102,7 @@ typedef enum
 #endif
 //Specify mode of operation, clock polarity and clock phase
 #ifndef LibSPI2_MODE
-#define LibSPI2_MODE		LibSPI_Mode_0
+#define LibSPI2_MODE        LibSPI_Mode_0
 #endif
 //----- SPI2 options end -------
 
@@ -119,8 +119,7 @@ void LibSPI_WriteMulti(SPI_TypeDef* SPIx, uint8_t* dataOut, uint32_t count);
 
 void LibSPI_ReadMulti(SPI_TypeDef* SPIx, uint8_t *dataIn, uint8_t dummy, uint32_t count);
 
-
-uint16_t LibSPI_Stop(SPI_TypeDef* SPIx);
+void LibSPI_Stop(SPI_TypeDef* SPIx);
 
 /* C++ detection */
 #ifdef __cplusplus
@@ -128,4 +127,3 @@ uint16_t LibSPI_Stop(SPI_TypeDef* SPIx);
 #endif
 
 #endif
-
