16f8915ef5e5bc8d5a0908e01dcbd8f76c7b8fe7
LDmicroGitHub <LDmicroGitHub@gmail.com>
2020-04-12 01:59:01 +0300

    add prototype of LIBRARIES_FOR ANSIC romlib.c

---
+++ ldmicro/LIBRARIES_FOR/ANSIC/romlib.c
@@ -0,0 +1,10 @@
+
+void EEPROM_write(unsigned char addr, unsigned char data)
+{
+    #warning You must provide an implementation of write EEPROM data for you target MCU or for an external EEPROM chip!
+}
+
+unsigned char EEPROM_read(int addr)
+{
+    #warning You must provide an implementation of reading EEPROM data for you target MCU or for an external EPPROM chip!
+}
