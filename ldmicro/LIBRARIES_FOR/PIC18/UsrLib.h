33b4e9cafe571d1a965c5437a5310b0eb6ee1e4e
LDmicroGitHub <LDmicroGitHub@gmail.com>
2021-05-23 01:00:20 +0300

    ldmicro/LIBRARIES_FOR/PIC18/UsrLib.h

--- ldmicro/LIBRARIES_FOR/PIC18/UsrLib.h
+++ ldmicro/LIBRARIES_FOR/PIC18/UsrLib.h
@@ -1,3 +1,5 @@
+#ifndef __USRLIB_H__
+#define __USRLIB_H__
 
 #include <htc.h>
 
@@ -21,13 +23,29 @@ void delay_us(const unsigned ud);       // ud must be constant
 void delay_ms(const unsigned md);       // md must be constant
 #endif
 
-#define delay_us(ud)    \
-    {   \
-    __delay_us(ud);     \
-    }
+#define delay_us(ud) __delay_us(ud)
+#define delay_ms(md) __delay_ms(md)
 
 
-#define delay_ms(md)    \
-    {   \
-    __delay_ms(md);     \
-    }
+#define PORTB_PULL_UPS_ON()  do { nRBPU = 0; } while(0)
+#define PORTB_PULL_UPS_OFF() do { nRBPU = 1; } while(0)
+
+#define PORT_BIT_SET(port, pin) PORT##port##bits.R##port##pin = 1
+// or
+//#define PORT_BIT_SET(port, pin) R##port##pin = 1 // deprecated warning
+
+#define PORT_BIT_CLEAR(port, pin) PORT##port##bits.R##port##pin = 0
+// or
+//#define PORT_BIT_CLEAR(port, pin) R##port##pin = 0 // deprecated warning
+
+#define PORT_BIT_STATE(port, pin) PORT##port##bits.R##port##pin
+// or
+//#define PORT_BIT_STATE(port, pin) R##port##pin // deprecated warning
+
+#define PIN_BIT_STATE(port, pin) PORT##port##bits.R##port##pin
+
+
+#define WDT_Init()    WDTCON = 1
+#define WDT_Restart() CLRWDT()
+
+#endif
